10b424c5e179ed89e3a6c4561e178852ff4417a3
Muhammad Fawwaz EFS <faw.edsa@gmail.com>
2024-10-30 15:48:33 +0700

    change colorscheme

--- lib/main.dart
+++ lib/main.dart
@@ -30,8 +30,8 @@ class MyApp extends StatelessWidget {
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSwatch(
-              primarySwatch: Colors.deepPurple,
-        ).copyWith(secondary: Colors.deepPurple[400]),      
+              primarySwatch: Colors.red,
+        ).copyWith(secondary: Colors.red[800]),      
       ),
       home: MyHomePage(),
     );
